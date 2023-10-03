a3297b206f7490469c9a7a1266920817f3756ece
Rao Awais <ra848683@gmail.com>
2023-10-03 19:12:14 +0500

    products picture handling corrected with sending data to backend

--- src/Pages/Product/addproduct.js
+++ src/Pages/Product/addproduct.js
@@ -10,11 +10,9 @@ import { useRef, useState } from 'react';
 import { addProduct } from '../../Services/Product';
 import axios from 'axios';
 
-
 const AddProduct = () => {
 
 const fileInput = useRef(null);
-const [images, setImages] = useState([null, null, null, null]);
 const [formData, setFormData] = useState({
     productName: '',
     price: '',
@@ -24,11 +22,8 @@ const [formData, setFormData] = useState({
     size2: '',
     size3: '',
     description: '',
-    mainimage: '',
-    image1: '',
-    image2: '',
-    image3: '',
-    image: '',
+    coverimage: '',
+    images: [],
 });
 
 const handlePhotoUpload = (index) => {
@@ -36,29 +31,25 @@ const handlePhotoUpload = (index) => {
     fileInput.current.click();
 }
 
-const imageDivRefs = [useRef(null), useRef(null), useRef(null)];
-
 const handleFileChange = (index, e) => {
-    const file = e.target.files[0];
-    const reader = new FileReader();
+    // const file = e.target.files[0];
+    // const reader = new FileReader();
   
-    reader.onloadend = () => {
-      const newImages = [...images];
-      newImages[index] = reader.result;
-      setImages(newImages);
+    // reader.onloadend = () => {
+    //   const newImages = [...images];
+    //   newImages[index] = reader.result;
+    //   setImages(newImages);
+    //     const imageDiv = document.getElementById(`imageDiv${index}`);
+    //   if (imageDiv) {
+    //     imageDiv.style.backgroundImage = `url(${reader.result})`;
+    //     imageDiv.style.backgroundSize = 'cover';
+    //     imageDiv.style.backgroundPosition = 'center';
+    //   }
+    // };
   
-      // Set the background image of the corresponding div
-      const imageDiv = document.getElementById(`imageDiv${index}`);
-      if (imageDiv) {
-        imageDiv.style.backgroundImage = `url(${reader.result})`;
-        imageDiv.style.backgroundSize = 'cover';
-        imageDiv.style.backgroundPosition = 'center';
-      }
-    };
-  
-    if (file) {
-      reader.readAsDataURL(file);
-    }
+    // if (file) {
+    //   reader.readAsDataURL(file);
+    // }
   };
   
 
@@ -70,32 +61,10 @@ const handleInputChange = (e) => {
     }));
 };
 
-
-const handleSubmit = async (e) => {
+const handleNewFormSubmit = async (e) => {
     e.preventDefault();
-    const imageAddresses = [];
-    console.log(images);
-    const uploadImages = async () => {
-        for (let i = 0; i < images.length; i++) {
-            if (images[i]) {
-                const formDataToSend = new FormData();
-                formDataToSend.append('image', images[i]);
-                console.log(formDataToSend.get('image'));
-                try {
-                    const response = await axios.post('http://localhost:3001/upload', formDataToSend);
-                    console.log(response.data.address);
-                    imageAddresses.push(response.data.address);
-                } catch (error) {
-                    console.error(error);
-                }
-            }
-        }
-    };
-
-    // Upload images and then send the form data with image addresses
-    await uploadImages();
-
-    const formDataToSend = new FormData();
+    console.log("Just before sending in function");
+    let formDataToSend = new FormData();
     formDataToSend.append('productName', formData.productName);
     formDataToSend.append('price', formData.price);
     formDataToSend.append('quantity', formData.quantity);
@@ -104,37 +73,28 @@ const handleSubmit = async (e) => {
     formDataToSend.append('size2', formData.size2);
     formDataToSend.append('size3', formData.size3);
     formDataToSend.append('description', formData.description);
-    formDataToSend.append('mainimage', images[0]);
-    formDataToSend.append('image1', images[1]);
-    formDataToSend.append('image2', images[2]);
-    formDataToSend.append('image3', images[3]);
-    formDataToSend.append('imageaddresses', JSON.stringify(imageAddresses));
-
-    try {
-        const response = await axios.post('http://localhost:3001/products/addproducts', formDataToSend);
-        return response.data;
-    } catch (error) {
-        console.error(error);
+    formDataToSend.append('images', formData.coverimage);
+    for (let i = 0; i < formData.images.length; i++) {
+        formDataToSend.append(`images`, formData.images[i]);
     }
-};
-
-const handleNewFormSubmit = async (e) => {
-    e.preventDefault();
-    console.log("Just before sending in function");
-    let formDataToSend = new FormData(document.getElementById("formID"));
-    console.log(formDataToSend);
+  
     try {
-      const response = await axios.post('http://localhost:3001/upload', formDataToSend);
-      console.log(response.data.address);
+      const response = await axios.post('http://localhost:3001/products/addproduct', formDataToSend);
+      console.log(response.data.filenames);
+      
     } catch (error) {
       console.error(error);
     }
+  
     e.target.reset();
     return false;
   };
   
 
-return (
+
+return (           
+     <form className='' onSubmit={handleNewFormSubmit} id="formID" enctype="multipart/form-data">
+
             <div className='flex h-screen'>
 
                 <Sidebar />
@@ -146,109 +106,37 @@ return (
                             <div className='p-10 flex justify-between items-center '>
                                 <h2 className='text-2xl font-bold'>Add Product</h2>
                                 <div className='flex justify-end items-center'>
-            <form className='' onSubmit={handleNewFormSubmit} id="formID" enctype="multipart/form-data">
 
-                    <div className="w-24 h-24 bg-gray-300 relative" id="imageDiv0">
-            <input
-            type="file"
-            className="absolute inset-0 opacity-0 w-full h-full cursor-pointer"
-            accept="image/png, image/jpg"
-            name="image"
-            id="file0"
-            onChange={(e) => handleFileChange(0, e)}
-            />
-            <label
-            className="absolute inset-0 flex items-center justify-center cursor-pointer"
-            htmlFor="file0"
-            >
-            Choose file
-            </label>
-        </div>
-        </form>
+                                {/* <div className="w-24 h-24 bg-gray-300 relative" id="imageDiv0">
+                            <input
+                            type="file"
+                            className="absolute inset-0 opacity-0 w-full h-full cursor-pointer"
+                            accept="image/png, image/jpg"
+                            name="image0"
+                            id="file0"
+                            onChange={(e) => handleFileChange(0, e)}
+                            />
+                            <label
+                            className="absolute inset-0 flex items-center justify-center cursor-pointer"
+                            htmlFor="file0"
+                            >
+                            Choose file
+                            </label>
+                        </div> */}
+                        <div className="col-span-1">
+                                <label htmlFor="image">Image</label>
+                                <input
+                                    type="file"
+                                    name="image"
+                                    id="image"
+                                    multiple
+                                    className="h-10 border p-1.5 mt-1 rounded px-4 w-full bg-gray-50"
+                                    onChange={(e) => {
+                                        setFormData({ ...formData, images: e.target.files })
+                                    }}
+                                />
 
-      <div className="w-24 h-24 bg-gray-300 relative" id="imageDiv1">
-        <input
-          type="file"
-          className="absolute inset-0 opacity-0 w-full h-full cursor-pointer"
-          accept="image/png, image/jpg"
-          name="image1"
-          id="file1"
-          onChange={(e) => handleFileChange(1, e) }
-        />
-        <label
-          className="absolute inset-0 flex items-center justify-center cursor-pointer"
-          htmlFor="file1"
-        >
-          Choose file
-        </label>
-      </div>
-
-      <div className="w-24 h-24 bg-gray-300 relative" id="imageDiv2">
-        <input
-          type="file"
-          className="absolute inset-0 opacity-0 w-full h-full cursor-pointer"
-          accept="image/png, image/jpg"
-          name="image2"
-          id="file2"
-          onChange={(e) => handleFileChange(2, e)}
-        />
-        <label
-          className="absolute inset-0 flex items-center justify-center cursor-pointer"
-          htmlFor="file2"
-        >
-          Choose file
-        </label>
-      </div>
-
-                                {/* <div className='w-24 h-24 bg-gray-300' onClick={() => handlePhotoUpload(0)} style={{ backgroundImage: `url(${images[0]})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
-                                            <div className=''>
-                                                <label className="custom-file-label" htmlFor="file" style={{ cursor: 'pointer' }}>
-                                                <input
-                                                    type="file"
-                                                    className="custom-file-input"
-                                                    id="file"
-                                                    accept='image/png; image/jpg'
-                                                    name="image"
-                                                    onChange={(e) => handleFileChange(0, e)}
-                                                    style={{ display: 'none' }}
-                                                />
-                                                Choose file
-                                                </label>
-                                            </div>
-                                            </div> */}
-
-                                            {/* <div className='w-24 h-24 bg-gray-300 mx-2' onClick={() => handlePhotoUpload(1)} style={{ backgroundImage: `url(${images[1]})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
-                                                    <div className=''>
-                                                        <label className="custom-file-label" htmlFor="file1" style={{ cursor: 'pointer' }}>
-                                                        <input
-                                                            type="file"
-                                                            className="custom-file-input"
-                                                            id="file1"
-                                                            accept='image/png; image/jpg'
-                                                            name="image1"
-                                                            onChange={(e) => handleFileChange(1, e)}
-                                                            style={{ display: 'none' }}
-                                                        />
-                                                        Choose file
-                                                        </label>
-                                                    </div>
-                                                    </div>
-                                                    <div className='w-24 h-24 bg-gray-300' onClick={() => handlePhotoUpload(2)} style={{ backgroundImage: `url(${images[2]})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
-                                                    <div className=''>
-                                                        <label className="custom-file-label" htmlFor="file2" style={{ cursor: 'pointer' }}>
-                                                        <input
-                                                            type="file"
-                                                            className="custom-file-input"
-                                                            id="file2"
-                                                            accept='image/png; image/jpg'
-                                                            name="image2"
-                                                            onChange={(e) => handleFileChange(2, e)}
-                                                            style={{ display: 'none' }}
-                                                        />
-                                                        Choose file
-                                                        </label>
-                                                    </div>
-                                                    </div> */}
+                  </div>
                                 </div>
                             </div>
                         </div>
@@ -285,56 +173,47 @@ return (
                                 />
                             </div>
                             <div className=''>
-                                <p className='text-gray-500 text-sm font-semibold pl-4'>Foto e produktit</p>
-                                {/* <div className='w-44 h-56 rounded-lg bg-gray-300 mx-2 pb-4' onClick={() => handlePhotoUpload(3)} style={{ backgroundImage: `url(${images[3]})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
-<div className='pl-5 pt-2'>
-<label className="custom-file-label" htmlFor="file3" style={{ cursor: 'pointer' }}>
-    <input
-    type="file"
-    className="custom-file-input"
-    id="file3"
-    accept='image/png; image/jpg'
-    name="image3"
-    onChange={(e) => handleFileChange(3, e)}
-    style={{ display: 'none' }}
+  {/* <p className='text-gray-500 text-sm font-semibold pl-4'>Foto e produktit</p> */}
+  <input
+      type="file"
+      className=""
+      accept="image/png, image/jpg"
+      name="coverImage"
+      id="coverImage"
+      onChange={(e) => setFormData({ ...formData, coverimage: e.target.files[0] })}
     />
-    Choose file
-</label>
+  {/* <div
+    className='w-44 h-56 rounded-lg bg-gray-300 mx-2 pb-4 relative'
+  >
+    <input
+      type="file"
+      className="absolute inset-0 opacity-0 w-full h-full cursor-pointer"
+      accept="image/png, image/jpg"
+      name="imagemain"
+      id="filemain"
+      onChange={(e) => handleFileChange(3, e)}
+    />
+    <label
+      className="absolute inset-0 flex items-center justify-center cursor-pointer"
+      htmlFor="filemain"
+      style={{ cursor: 'pointer' }}
+    >
+      Choose file
+    </label>
+  </div> */}
+  <div className='pl-5 pt-2'>
+    <img src={AddPhoto} alt='Product' className=' border border-teal-500 rounded-lg w-[150px] h-[50px] mb-4 ' />
+  </div>
 </div>
-</div> */}
-                                                        <div className='pl-5 pt-2'>
-                                    <img src={AddPhoto} alt='Product' className=' border border-teal-500 rounded-lg w-[150px] h-[50px] mb-4 ' />
-                                </div>
-                            </div>
+
                         </div>
 
                     </div>
                 </div>
                 <input type="file" ref={fileInput} style={{ display: 'none' }} onChange={handleFileChange} />
             </div>       
-            //  </form>
-            
+             </form>
 
 )
 }
-
 export default AddProduct;
-
-
-
-
-        {/* <form className='' onSubmit={handleNewFormSubmit} id="formID" enctype="multipart/form-data">
-            <div className=''>
-
-                <div className="input-group mb-3">
-                    <div className="custom-file py-1">
-                        <input type="file" className="custom-file-input" id="file" accept='image/png; image/jpg' name="image"  />
-                        <div className="custom-file-label" for="file"></div>
-                    </div>
-                </div>
-            </div>
-
-            <div className='d-flex justify-content-end'>
-                <button type='submit' className='btn btn-primary'>Send</button>
-            </div>
-        </form> */}
