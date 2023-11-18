1a13ad446df3b4da6cd4399ac9c019bae0107ae8
m-khizerr <i200773@nu.edu.pk>
2023-11-18 16:15:20 +0500

    route fix

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
       <SideBar/>
       <NavBar />
       <Routes>
-        <Route path="/home5" element={<Dashboard />} />
+        <Route path="/home" element={<Dashboard />} />
         <Route path="/signin" element={<Login />} />
         <Route path="/products" element={<Products />} />
           <Route path="/products/add" element={<AddProduct/>}/>
