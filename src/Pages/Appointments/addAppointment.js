2f6d66271147ce17768d05cd026c42ea551aa945
Daimhoti9e <daimhoti8@gmail.com>
2024-01-17 15:28:16 +0100

    new

--- src/Pages/Appointments/addAppointment.js
+++ src/Pages/Appointments/addAppointment.js
@@ -1,173 +1,99 @@
-import { useState } from "react";
+import {useState} from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 
 export const AddAppointment = () => {
-  const [formData, setFormData] = useState({
-    name: "",
-    price: "",
-    duration: "",
-    group: "",
-    displayGroup: "",
-    onlyParent: false,
-  });
 
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-
-  const navigate = useNavigate();
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    if (
-      formData.service === "" ||
-      formData.price === "" ||
-      formData.duration === "" ||
-      formData.group === ""
-    ) {
-      alert("Ju lutem mbushni të gjitha fushat!");
-      return;
-    }
-
-    const response = await axios.post(
-      `${process.env.REACT_APP_BACKEND_URL}/service/addservice`,
-      formData
-    );
-    if (response.status === 201) {
-      setFormData({
-        service: "",
+    const [formData, setFormData] = useState({
+        name: "",
         price: "",
         duration: "",
-        category: "",
-      });
-      navigate("/appointments");
-    } else {
-      alert("Ka ndodhur një gabim!");
-    }
-  };
+        group: "",
+        displayGroup: "",
+        onlyParent: false,
+    });
 
-  const groups = [
-    "Për Fëmijë",
-    "Për Bebe",
-    "Mami + Bebi",
-    "Group Plush",
-    "Për Nënen",
-  ];
-  const displayGroups = [
-    "Hidroterapi",
-    "Masazh për bebe",
-    "Plush",
-    "VIP Plush",
-    "MAMI + BEBI",
-    "Masazh për nëna shtatzëna dhe pas lindjes",
-  ];
+    const handleChange = (e) => {
+        setFormData({...formData, [e.target.name]: e.target.value});
+    };
 
-  return (
-    <>
-    <div className="sm:pl-0 pl-16">
-      <div className="flex flex-col justify-center items-center min-h-screen bg-gray-100">
-        <h1 className="sm:text-4xl text-2xl font-bold text-center mt-10 mb-5">
-          Shto një Service
-        </h1>
-        <form
-          onSubmit={handleSubmit}
-          className="flex flex-col justify-center items-center"
-        >
-          <div className="flex flex-col justify-center items-center">
-            <label htmlFor="name" className="text-xl font-bold">
-              Shërbimi
-            </label>
-            <input
-              type="text"
-              name="name"
-              id="name"
-              className="border-2 border-black rounded-md p-2"
-              onChange={handleChange}
-            />
-          </div>
-          <div className="flex flex-col justify-center items-center">
-            <label htmlFor="price" className="text-xl font-bold">
-              Çmimi
-            </label>
-            <input
-              type="number"
-              name="price"
-              id="price"
-              className="border-2 border-black rounded-md p-2"
-              onChange={handleChange}
-            />
-          </div>
-          <div className="flex flex-col justify-center items-center">
-            <label htmlFor="duration" className="text-xl font-bold">
-              Koha e shërbimit
-            </label>
-            <input
-              type="number"
-              name="duration"
-              id="duration"
-              className="border-2 border-black rounded-md p-2"
-              onChange={handleChange}
-            />
-          </div>
-          <div className="flex flex-col justify-center items-center">
-            <label htmlFor="category" className="text-xl font-bold">
-              Kategoria
-            </label>
-            <select
-              name="group"
-              id="group"
-              className="border-2 border-black rounded-md p-2"
-              onChange={handleChange}
-            >
-              <option value="" disabled selected>
-                Zgjidhni një kategori
-              </option>
-              {groups.map((group) => (
-                <option value={group}>{group}</option>
-              ))}
-            </select>
-          </div>
-          <div className="flex flex-col justify-center items-center">
-            <label htmlFor="displayGroup" className="text-xl font-bold">
-              Kategoria e shfaqur
-            </label>
-            <select
-              name="displayGroup"
-              id="displayGroup"
-              className="border-2 border-black rounded-md p-2"
-              onChange={handleChange}
-            >
-              <option value="" disabled selected>
-                Zgjidhni një kategori
-              </option>
-              {displayGroups.map((group) => (
-                <option value={group}>{group}</option>
-              ))}
-            </select>
-          </div>
-          <div className="flex flex-col justify-center items-center">
-            <label htmlFor="onlyParent" className="text-xl font-bold">
-              Vetëm prind
-            </label>
-            <input
-              type="checkbox"
-              name="onlyParent"
-              id="onlyParent"
-              className="border-2 border-black rounded-md p-2"
-              onChange={handleChange}
-            />
-          </div>
-          <button
-            type="submit"
-            className="border-2 border-black rounded-md p-2 mt-5"
-          >
-            Shto Service
-          </button>
-        </form>
-      </div>
-      </div>
-    </>
-  );
-};
+    const navigate = useNavigate()
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if (formData.service === "" || formData.price === "" || formData.duration === "" || formData.group === "") {
+            alert("Ju lutem mbushni të gjitha fushat!")
+            return;
+        }
+
+        const response = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/service/addservice`, formData);
+        if (response.status === 201) {
+            setFormData({
+                service: "",
+                price: "",
+                duration: "",
+                category: "",
+            })
+            navigate('/appointments')
+        } else {
+            alert("Ka ndodhur një gabim!")
+        }
+    };
+
+    const groups = ["Për Fëmijë", "Për Bebe", "Mami + Bebi", "Group Plush", "Për Nënen"];
+    const displayGroups= ["Hidroterapi", "Masazh për bebe", "Plush", "VIP Plush", "MAMI + BEBI","Masazh për nëna shtatzëna dhe pas lindjes"];
+
+    return (
+        <>
+            <div className="flex flex-col justify-center items-center min-h-screen">
+                <h1 className="text-4xl font-bold text-center mt-10 mb-5">Shto një Service</h1>
+                <form onSubmit={handleSubmit} className="flex flex-col justify-center items-center">
+                    <div className="flex flex-col justify-center items-center">
+                        <label htmlFor="name" className="text-xl font-bold">Shërbimi</label>
+                        <input type="text" name="name" id="name" className="border-2 border-black rounded-md p-2"
+                               onChange={handleChange}/>
+                    </div>
+                    <div className="flex flex-col justify-center items-center">
+                        <label htmlFor="price" className="text-xl font-bold">Çmimi</label>
+                        <input type="number" name="price" id="price" className="border-2 border-black rounded-md p-2"
+                               onChange={handleChange}/>
+                    </div>
+                    <div className="flex flex-col justify-center items-center">
+                        <label htmlFor="duration" className="text-xl font-bold">Koha e shërbimit</label>
+                        <input type="number" name="duration" id="duration"
+                               className="border-2 border-black rounded-md p-2"
+                               onChange={handleChange}/>
+                    </div>
+                    <div className="flex flex-col justify-center items-center">
+                        <label htmlFor="category" className="text-xl font-bold">Kategoria</label>
+                        <select name="group" id="group" className="border-2 border-black rounded-md p-2"
+                                onChange={handleChange}>
+                            <option value="" disabled selected>Zgjidhni një kategori</option>
+                            {groups.map((group) => (
+                                <option value={group}>{group}</option>
+                            ))}
+                        </select>
+                    </div>
+                    <div className="flex flex-col justify-center items-center">
+                        <label htmlFor="displayGroup" className="text-xl font-bold">Kategoria e shfaqur</label>
+                        <select name="displayGroup" id="displayGroup" className="border-2 border-black rounded-md p-2"
+                                onChange={handleChange}>
+                            <option value="" disabled selected>Zgjidhni një kategori</option>
+                            {displayGroups.map((group) => (
+                                <option value={group}>{group}</option>
+                            ))}
+                        </select>
+                    </div>
+                    <div className="flex flex-col justify-center items-center">
+                        <label htmlFor="onlyParent" className="text-xl font-bold">Vetëm prind</label>
+                        <input type="checkbox" name="onlyParent" id="onlyParent"
+                               className="border-2 border-black rounded-md p-2"
+                               onChange={handleChange}/>
+                    </div>
+                    <button type="submit" className="border-2 border-black rounded-md p-2 mt-5">Shto Service</button>
+                </form>
+            </div>
+        </>
+    )
+}
--- src/Pages/Appointments/editAppointment.js
+++ src/Pages/Appointments/editAppointment.js
@@ -1,201 +1,117 @@
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 import axios from "axios";
-import { useNavigate, useParams } from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
 
 export const EditAppointment = () => {
-  const { id } = useParams();
 
-  const [formData, setFormData] = useState({
-    name: "",
-    price: "",
-    duration: "",
-    group: "",
-    displayGroup: "",
-    onlyParent: false,
-  });
+    const {id} = useParams()
 
-  useEffect(() => {
-    if (localStorage.getItem("token") === null) {
-      navigate("/login");
-    }
-    if (localStorage.getItem("role") !== "admin") {
-      navigate("/employees");
-    }
-  }, []);
+    const [formData, setFormData] = useState({
+        name: "",
+        price: "",
+        duration: "",
+        group: "",
+        displayGroup: "",
+        onlyParent: false,
+    });
 
-  useEffect(() => {
-    const fetchEmployeeDetails = async () => {
-      try {
-        console.log(id);
-        const response = await axios.get(
-          `${process.env.REACT_APP_BACKEND_URL}/service/getservice/${id}`
-        );
-        setFormData(response.data);
-      } catch (error) {
-        console.error("Error fetching employee details:", error.message);
-      }
+    useEffect(() => {
+        if(localStorage.getItem('token') === null){
+            navigate('/login')
+        }
+        if(localStorage.getItem('role') !== 'admin'){
+            navigate('/employees')
+        }
+    }, []);
+
+    useEffect(() => {
+        const fetchEmployeeDetails = async () => {
+            try {
+                console.log(id);
+                const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/service/getservice/${id}`);
+                setFormData(response.data);
+            } catch (error) {
+                console.error('Error fetching employee details:', error.message);
+            }
+        };
+        fetchEmployeeDetails();
+    }, [id]);
+
+    const handleChange = (e) => {
+        console.log(e.target.value)
+        setFormData({...formData, [e.target.name]: e.target.value});
     };
-    fetchEmployeeDetails();
-  }, [id]);
 
-  const handleChange = (e) => {
-    console.log(e.target.value);
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
+    const navigate = useNavigate()
 
-  const navigate = useNavigate();
+    const handleSubmit = async (e) => {
+        e.preventDefault();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+        if (formData.service === "" || formData.price === "" || formData.duration === "" || formData.group === "") {
+            alert("Ju lutem mbushni të gjitha fushat!")
+            return;
+        }
+        const response = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/service/editservice/${id}`, formData);
+        if (response.status === 200) {
+            navigate('/appointments')
+        } else {
+            alert("Ka ndodhur një gabim!")
+        }
+    };
 
-    if (
-      formData.service === "" ||
-      formData.price === "" ||
-      formData.duration === "" ||
-      formData.group === ""
-    ) {
-      alert("Ju lutem mbushni të gjitha fushat!");
-      return;
-    }
-    const response = await axios.post(
-      `${process.env.REACT_APP_BACKEND_URL}/service/editservice/${id}`,
-      formData
-    );
-    if (response.status === 200) {
-      navigate("/appointments");
-    } else {
-      alert("Ka ndodhur një gabim!");
-    }
-  };
+    const groups = ["Për Fëmijë", "Për Bebe", "Mami + Bebi", "Group Plush", "Për Nënen"];
+    const displayGroups= ["Hidroterapi", "Masazh për bebe", "Plush", "VIP Plush", "MAMI + BEBI","Masazh për nëna shtatzëna dhe pas lindjes"];
 
-  const groups = [
-    "Për Fëmijë",
-    "Për Bebe",
-    "Mami + Bebi",
-    "Group Plush",
-    "Për Nënen",
-  ];
-  const displayGroups = [
-    "Hidroterapi",
-    "Masazh për bebe",
-    "Plush",
-    "VIP Plush",
-    "MAMI + BEBI",
-    "Masazh për nëna shtatzëna dhe pas lindjes",
-  ];
-
-  return (
-    <>
-    <div className="sm:pl-0 pl-16">
-      <div className="flex flex-col justify-center items-center min-h-screen">
-        <h1 className="sm:text-4xl text-2xl font-bold text-center mt-10 mb-5">
-          Shto një Service
-        </h1>
-        <form
-          onSubmit={handleSubmit}
-          className="flex flex-col justify-center items-center"
-        >
-          <div className="flex flex-col justify-center items-center">
-            <label htmlFor="name" className="text-xl font-bold">
-              Shërbimi
-            </label>
-            <input
-              value={formData.name}
-              type="text"
-              name="name"
-              id="name"
-              className="border-2 border-black rounded-md p-2"
-              onChange={handleChange}
-            />
-          </div>
-          <div className="flex flex-col justify-center items-center">
-            <label htmlFor="price" className="text-xl font-bold">
-              Çmimi
-            </label>
-            <input
-              value={formData.price}
-              type="number"
-              name="price"
-              id="price"
-              className="border-2 border-black rounded-md p-2"
-              onChange={handleChange}
-            />
-          </div>
-          <div className="flex flex-col justify-center items-center">
-            <label htmlFor="duration" className="text-xl font-bold">
-              Koha e shërbimit
-            </label>
-            <input
-              value={formData.duration}
-              type="number"
-              name="duration"
-              id="duration"
-              className="border-2 border-black rounded-md p-2"
-              onChange={handleChange}
-            />
-          </div>
-          <div className="flex flex-col justify-center items-center">
-            <label htmlFor="category" className="text-xl font-bold">
-              Kategoria
-            </label>
-            <select
-              value={formData.group}
-              name="group"
-              id="group"
-              className="border-2 border-black rounded-md p-2"
-              onChange={handleChange}
-            >
-              <option value="" disabled selected>
-                Zgjidhni një kategori
-              </option>
-              {groups.map((group) => (
-                <option value={group}>{group}</option>
-              ))}
-            </select>
-          </div>
-          <div className="flex flex-col justify-center items-center">
-            <label htmlFor="displayGroup" className="text-xl font-bold">
-              Kategoria e shfaqur
-            </label>
-            <select
-              value={formData.displayGroup}
-              name="displayGroup"
-              id="displayGroup"
-              className="border-2 border-black rounded-md p-2"
-              onChange={handleChange}
-            >
-              <option value="" disabled selected>
-                Zgjidhni një kategori
-              </option>
-              {displayGroups.map((group) => (
-                <option value={group}>{group}</option>
-              ))}
-            </select>
-          </div>
-          <div className="flex flex-col justify-center items-center">
-            <label htmlFor="onlyParent" className="text-xl font-bold">
-              Vetëm prind
-            </label>
-            <input
-              checked={formData.onlyParent}
-              type="checkbox"
-              name="onlyParent"
-              id="onlyParent"
-              className="border-2 border-black rounded-md p-2"
-              onChange={(e) =>
-                setFormData({ ...formData, onlyParent: e.target.checked })
-              }
-            />
-          </div>
-          <button
-            type="submit"
-            className="border-2 border-black rounded-md p-2 mt-5"
-          >
-            Shto Service
-          </button>
-        </form>
-      </div>
-      </div>
-    </>
-  );
-};
+    return (
+        <>
+            <div className="flex flex-col justify-center items-center min-h-screen">
+                <h1 className="text-4xl font-bold text-center mt-10 mb-5">Shto një Service</h1>
+                <form onSubmit={handleSubmit} className="flex flex-col justify-center items-center">
+                    <div className="flex flex-col justify-center items-center">
+                        <label htmlFor="name" className="text-xl font-bold">Shërbimi</label>
+                        <input value={formData.name} type="text" name="name" id="name" className="border-2 border-black rounded-md p-2"
+                               onChange={handleChange}/>
+                    </div>
+                    <div className="flex flex-col justify-center items-center">
+                        <label htmlFor="price" className="text-xl font-bold">Çmimi</label>
+                        <input value={formData.price} type="number" name="price" id="price" className="border-2 border-black rounded-md p-2"
+                               onChange={handleChange}/>
+                    </div>
+                    <div className="flex flex-col justify-center items-center">
+                        <label htmlFor="duration" className="text-xl font-bold">Koha e shërbimit</label>
+                        <input value={formData.duration} type="number" name="duration" id="duration"
+                               className="border-2 border-black rounded-md p-2"
+                               onChange={handleChange}/>
+                    </div>
+                    <div className="flex flex-col justify-center items-center">
+                        <label htmlFor="category" className="text-xl font-bold">Kategoria</label>
+                        <select value={formData.group} name="group" id="group" className="border-2 border-black rounded-md p-2"
+                                onChange={handleChange}>
+                            <option value="" disabled selected>Zgjidhni një kategori</option>
+                            {groups.map((group) => (
+                                <option value={group}>{group}</option>
+                            ))}
+                        </select>
+                    </div>
+                    <div className="flex flex-col justify-center items-center">
+                        <label htmlFor="displayGroup" className="text-xl font-bold">Kategoria e shfaqur</label>
+                        <select value={formData.displayGroup} name="displayGroup" id="displayGroup" className="border-2 border-black rounded-md p-2"
+                                onChange={handleChange}>
+                            <option value="" disabled selected>Zgjidhni një kategori</option>
+                            {displayGroups.map((group) => (
+                                <option value={group}>{group}</option>
+                            ))}
+                        </select>
+                    </div>
+                    <div className="flex flex-col justify-center items-center">
+                        <label htmlFor="onlyParent" className="text-xl font-bold">Vetëm prind</label>
+                        <input checked={formData.onlyParent} type="checkbox" name="onlyParent" id="onlyParent"
+                               className="border-2 border-black rounded-md p-2"
+                               onChange={e=>setFormData({...formData, onlyParent: e.target.checked})}/>
+                    </div>
+                    <button type="submit" className="border-2 border-black rounded-md p-2 mt-5">Shto Service</button>
+                </form>
+            </div>
+        </>
+    )
+}
