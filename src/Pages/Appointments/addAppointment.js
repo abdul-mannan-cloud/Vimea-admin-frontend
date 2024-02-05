88252881c5a033e595632f2d884a45b2d29788e1
Daimhoti9 <73882220+Daimhoti9@users.noreply.github.com>
2024-02-05 21:46:43 +0100

    Update addAppointment.js

--- src/Pages/Appointments/addAppointment.js
+++ src/Pages/Appointments/addAppointment.js
@@ -45,7 +45,7 @@ export const AddAppointment = () => {
 
     const groups = ["Për Fëmijë", "Për Bebe", "Mami + Bebi", "Group Plush", "Për Nënen"];
     const displayGroups= ["HYDROTHERAPY", "BABY MASSAGE", "PLUSH", "VIP PLUSH", "MOM + BABY","MASSAGE FOR MOTHER"];
-    const displayGroupsAlb= ["HIDROTERAPI", "MASAZHA PËR BEBE", "PLUSH", "VIP PLUSH", "MAMI + BEBI","MASAZHË PËR NËNA"];
+    const displayGroupsAlb= ["HIDROTERAPI", "MASAZHË PËR BEBE", "PLUSH", "VIP PLUSH", "MAMI + BEBI","MASAZHË PËR NËNA"];
 
     return (
         <>
